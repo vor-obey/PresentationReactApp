3bcf07b35bfd2d5a4f276d0180abcf568570d456
vor-obey <vv1641627@gmail.com>
2020-08-02 21:50:41 +0300

    Added Material UI library

--- src/constants.js
+++ src/constants.js
@@ -1,20 +1,17 @@
 import React from "react";
 
 //------------------Items for navigation menu---------------------
-export const MENU_ITEM_All = ['Productions', 'Features', 'Prising', 'Help', 'Contacts'];
+export const MENU_ITEM_All = ["Productions", "Features", "Prising", "Help", "Contacts"];
 
 //---------------------Links for footer----------------------------
 
-export const footerLinksLearnMore = ['How it works?', 'Meeting tools', 'Live striming', 'Contact method'];
-export const footerLinksAboutUs = ['About us', 'Privacy police', 'Features', 'Terms & Conditions'];
-export const footerLinksSupport = ['F.A.Q.', 'Contact us', 'Live chat', 'Phone call'];
+export const footerLinksLearnMore = ["How it works?", "Meeting tools", "Live striming", "Contact method"];
+export const footerLinksAboutUs = ["About us", "Privacy police", "Features", "Terms & Conditions"];
+export const footerLinksSupport = ["F.A.Q.", "Contact us", "Live chat", "Phone call"];
 
 //------------------Items for product package---------------------
 
-export const arrayItem_1 = ['Unlimited events', 'Connect Dropbox & Evernote'];
-export const arrayItem_2 = ['Unlimited events', 'Connect Dropbox & Evernote'];
+export const arrayItem_1 = ["Unlimited events", "Connect Dropbox & Evernote"];
+export const arrayItem_2 = ["Unlimited events", "Connect Dropbox & Evernote"];
 
-
-
-export const arrowIcon = <i className="fas fa-arrow-right"/>;
 
